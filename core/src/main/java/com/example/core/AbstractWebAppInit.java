362c934fd3b5b71e36711e1c03ce5bfd0d556c2f
Olaf Krische <public@ecopatz.de>
2013-10-11 12:02:03 +0200

    WebAppInit now without explicitly implementing WebAppInitializer

--- core/src/main/java/com/example/core/AbstractWebAppInit.java
+++ core/src/main/java/com/example/core/AbstractWebAppInit.java
@@ -3,7 +3,9 @@ package com.example.core;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-public abstract class AbstractWebAppInit {
+import org.springframework.web.WebApplicationInitializer;
+
+public abstract class AbstractWebAppInit implements WebApplicationInitializer {
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		System.out.println("onStartup from here: " + AbstractWebAppInit.class.getName());
 	}
--- webapp/src/main/java/com/example/webapp/WebAppInit.java
+++ webapp/src/main/java/com/example/webapp/WebAppInit.java
@@ -4,5 +4,6 @@ import org.springframework.web.WebApplicationInitializer;
 
 import com.example.core.AbstractWebAppInit;
 
-public class WebAppInit extends AbstractWebAppInit implements WebApplicationInitializer {
+public class WebAppInit extends AbstractWebAppInit {
+//public class WebAppInit extends AbstractWebAppInit implements WebApplicationInitializer {
 }
